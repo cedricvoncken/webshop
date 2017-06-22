12c0a07db39d46b16aeb9f07c1b1f23f7eaa3d45
cedricvoncken <cedric.voncken@student.hu.nl>
2017-06-22 10:37:03 +0200

    Update bestellen.js

--- src/main/webapp/javascript/bestellen.js
+++ src/main/webapp/javascript/bestellen.js
@@ -80,7 +80,8 @@ $("#post").click(function(){
 });
 
 function besteld() {
-	alert("Order is verzonden! Zodra u het geld overmaakt naar:\n NL99 ABNA 0123 4567 89 \nzal de order verder verwerkt worden! Als er binnen 2 weken nog geen geen geld is overgemaakt zal de order verwijdert worden. Voor meer informatie zie de pagina 'levering'");
+	var user2 = JSON.parse(window.sessionStorage.getItem("bestelling"));
+	alert("Order is verzonden! Zodra u het geld:\n "+ user2.prijs +" overmaakt naar:\n NL99 ABNA 0123 4567 89 \nzal de order verder verwerkt worden! Als er binnen 2 weken nog geen geen geld is overgemaakt zal de order verwijdert worden. Voor meer informatie zie de pagina 'levering'");
 	window.location.href = "/index.html";
 }
 
